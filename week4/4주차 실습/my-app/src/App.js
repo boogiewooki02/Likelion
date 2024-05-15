51928a4bfd112664235e42bfe3e7e6f3a91a1ea2
dong-wook <boogiewooki02@gmail.com>
2024-05-15 17:28:49 +0900

    4주차 실습 제출

---
+++ week4/4주차 실습/my-app/src/App.js
@@ -0,0 +1,12 @@
+import Screen from "./Screen";
+
+
+function App() {
+  return (
+    <>
+      <Screen />
+    </>
+  );
+}
+
+export default App;
---
+++ week4/4주차 실습/my-app/src/Screen.jsx
@@ -0,0 +1,13 @@
+import Header from "./Header";
+import Main from "./Main";
+
+function Screen() {
+  return (
+    <div>
+      <Header />
+      <Main />
+    </div>
+  );
+}
+
+export default Screen;
